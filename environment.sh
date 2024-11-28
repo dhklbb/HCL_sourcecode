33e2de4b1be4aa1e3d2e25f3cc01f0c59849178c
ubuntu <2320271312@qq.com>
2024-11-28 19:23:21 +0800

    environment

---
+++ environment.sh
@@ -0,0 +1,4 @@
+wget https://sis-sdk-repository.obs.cn-north-1.myhuaweicloud.com/python/huaweicloud-python-sdk-sis-1.8.3.zip
+unzip huaweicloud-python-sdk-sis-1.8.3.zip
+pip install ./huaweicloud-python-sdk-sis-1.8.3
+pip install -r requirements.txt
